c04b845a4f471188cd369a1dcd55f972d3470bd5
arnav2000agr <92395924+arnav2000agr@users.noreply.github.com>
2023-12-15 04:02:31 +0530

    Merged

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
 import Homepage from './Components/Homepage';
 import Login from './Pages/Login';
 import Register from './Pages/Register';
+import MyForm from './Pages/Form';
 
 const App = () => {
   return (
-    <div className="w-full">
+    <div className="">
       <Router>
         <div className="">
         <Routes>
             <Route path="/" element={<Homepage />} />
             <Route path="/login" element={<Login/>}/>
+            <Route path="/register" element={<Register/>}/>
+            <Route path="/form" element={<MyForm/>}/>
         </Routes>
         </div>
       </Router>
