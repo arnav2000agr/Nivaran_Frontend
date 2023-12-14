0b39e660837a316b094f04cb1720d90df8105453
arnav2000agr <92395924+arnav2000agr@users.noreply.github.com>
2023-12-14 21:27:36 +0530

    edits

---
+++ src/Components/Homepage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './Header'
+import Carousel1 from './Carousel1'
+
+function Homepage() {
+  return (
+    <>
+    <div className='flex flex-col'>
+        <div><Header/></div>
+        <div><Carousel1/></div>
+    </div>
+        
+    </>
+  )
+}
+
+export default Homepage
