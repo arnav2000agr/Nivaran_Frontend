6b2f4c093d86134003e8e135116aabf7a737ba98
arnav2000agr <92395924+arnav2000agr@users.noreply.github.com>
2023-12-14 21:26:26 +0530

    Edits

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
 import Header from './Components/Header';
-import Login from './Pages/Login';
+import Homepage from './Components/Homepage';
+import Carousel from './Components/Carousel1';
 
-function App() {
+const App = () => {
   return (
-    <div className="m-0 p-0">
-        <Login/>
+    <div className="App">
+      <Router>
+        <div className="container">
+        <Routes>
+            <Route path="/" element={<Homepage />} />
+        </Routes>
+        </div>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
+// import './App.css';
+// import Header from './Components/Header';
+// import Login from './Pages/Login';
+
+// function App() {
+//   return (
+//     <div className="h-screen w-screen flex flex-col">
+//       <div className='sticky'>
+//         <Header/>
+//       </div>
+//       <Login/>
+//     </div>
+//   );
+// }
+
+// export default App;
